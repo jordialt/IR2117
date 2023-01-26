ea60bf816d23034feef8bc81bf68ab8d28e93427
jordialt <al416445@uji.es>
2023-01-26 12:52:39 +0100

    V1

---
+++ Sum/sum.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int suma=0
+int num;
+int main(){
+	std::cout<<"Introduce un número: ";
+	std::cin<<num;
+	std::cout<<std::endl;
+	std::cout<<"The sum from 1 to "<<num<<"is: "<<sum;
