4105eec435ab491723c7e2d6c398f673ec834483
jordialt <al416445@uji.es>
2023-01-26 13:18:47 +0100

    V4

--- Sum/sum.cpp
+++ Sum/sum.cpp
@@ -11,6 +11,11 @@ int main(){
 	std::cout<<"Introduce un número: ";
 	std::cin>>num;
 	std::cout<<std::endl;
-	std::cout<<"The sum from 1 to "<<num<<" is: "<<suma(num)<<std::endl;
+	if(num>=1){
+		std::cout<<"The sum from 1 to "<<num<<" is: "<<suma(num)<<std::endl;
+	
+	}else{
+		std::cout<<"El número debe ser mayor o igual a 1";
+	}
 	return 0;
 }
