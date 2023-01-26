cdafe046c0b1f4ef5dc7120f00a0f4aff2b5ca29
jordialt <al416445@uji.es>
2023-01-26 12:30:37 +0100

    V1

--- HelloWorld/hello.cpp
+++ HelloWorld/hello.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
+#include <string>
 int main(){
-	std::cout<<"Hello World!" << std::endl;
+	std::string name;
+	std::cout<<"What is your name?" << std::endl;
+	std::cin>>name;
+	std::cout<<"Hello "<<name<<"!"<<std::endl;
 	return 0;
 }
