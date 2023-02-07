e9b65d260360bc59eb30784f05605314f2996961
jordialt <al416445@uji.es>
2023-02-07 12:50:32 +0100

    V1

---
+++ quadratic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    double a,b,c,x1,x2;
+    std::cout<<"Enter a: ";
+    std::cin>>a;
+    std::cout<<"Enter b: ";
+    std::cin>>b;
+    std::cout<<"Enter c: ";
+    std::cin>>c;
+    x1=0;
+    x2=0;
+    std::cout<<"x1= "<<x1<<std::endl;
+    std::cout<<"x2= "<<x2<<std::endl;
+    return 0;
+}
