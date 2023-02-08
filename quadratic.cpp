88910bfee966351a1ffdeea8b4c7d9cd0c09abfc
jordialt <al416445@uji.es>
2023-02-08 17:37:38 +0100

    V5

--- quadratic.cpp
+++ quadratic.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <cmath>
+#include <complex>
 
 int main(){
-    double a,b,c,x1,x2;
+    double a,b,c;
+    std::complex<double> x1,x2; 
+    
     std::cout<<"Enter a: ";
     std::cin>>a;
     std::cout<<"Enter b: ";
@@ -11,7 +14,11 @@ int main(){
     std::cin>>c;
     double discriminant= b*b-4*a*c;
     if (discriminant<0){
-        std::cout<<"There are no real solutions";
+        std::complex<double> sqrt_discriminant(0, sqrt(-discriminant));
+        x1=(-b+sqrt_discriminant)/(2*a);
+        x2=(-b-sqrt_discriminant)/(2*a);
+        std::cout<<"x1= "<<x1<<std::endl;
+        std::cout<<"x2= "<<x2<<std::endl;
     }
     else{
         if(discriminant==0){
