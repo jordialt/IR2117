8d76f22b37ff80c61ca458e983820d1664270f18
jordialt <al416445@uji.es>
2023-02-14 12:23:24 +0100

    V7 mean_arith

--- arith_mean.cpp
+++ arith_mean.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
+#include <vector>
 int main() {
     double m=0, s=0;
-    int n=0, num;
-    std::cin>>num;
+    int n=0, element;
+    std::vector<int> elements;
+    std::cin>>element;
     while(!std::cin.eof()){
-        s+=num;
+        elements.push_back(element);
         n++;
-        std::cin>>num;
+        std::cin>>element;
     }
     
-
+    for (int i:elements)
+        s+= i;
     m= s/n;
     std::cout<< "Arithmetic mean: " << m << std::endl;
     return 0;
