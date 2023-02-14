b6abce131d8928959c4e8fe9e297a545280bd57f
jordialt <al416445@uji.es>
2023-02-14 12:59:48 +0100

    Moda V3

--- mode.cpp
+++ mode.cpp
@@ -11,17 +11,20 @@ int main() {
         std::cin>>element;
     }
     int n=elements.size();
-    int count[n];
+    int count[n],max=0;
     for(int j=0; j<n; j++){
         count[j]=0;
         for (int i=0;i<n;i++){
             if(elements[i]==elements[j]){
                 count[j]++;
             }
+        if(count[j]>count[max])
+            max=j;
         }
     
     }
-    
+
+    m=elements[max];
     std::cout<< "Mode: " << m << std::endl;
     return 0;
 }
