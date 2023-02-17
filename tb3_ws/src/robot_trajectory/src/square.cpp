c07a81b0118a1e181dcd2bfe13d99d9955ce8009
jordialt <al416445@uji.es>
2023-02-17 19:02:19 +0100

    Square challenge

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -11,20 +11,38 @@ int main(int argc, char * argv[])
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
   geometry_msgs::msg::Twist message;
   rclcpp::WallRate loop_rate(10ms);
-  int i=0,n=1000;
-  while (rclcpp::ok() && (i<n)) {
-	i++;
-  	message.linear.x = 0.1;
-    publisher->publish(message);
-    rclcpp::spin_some(node);
-    loop_rate.sleep();
+  for(int j=0;j<4;j++){
+  	int i=0,n=1150;
+  	while (rclcpp::ok() && (i<n)) {
+		i++;
+  		message.linear.x = 0.10;
+    	publisher->publish(message);
+    	rclcpp::spin_some(node);
+    	loop_rate.sleep();
+  	}
+  	//send zero velocity to topic
+  	message.linear.x = 0.0;
+  	publisher->publish(message);
+  	rclcpp::spin_some(node);
+  	loop_rate.sleep();
+  
+  	i=0;
+  	n=1150;
+
+  	while (rclcpp::ok() && (i<n)) {
+		i++;
+  		message.angular.z = 0.15707963268;
+    	publisher->publish(message);
+    	rclcpp::spin_some(node);
+    	loop_rate.sleep();
+  	}
+  	message.angular.z = 0.0;
+  	publisher->publish(message);
+  	rclcpp::spin_some(node);
+  	loop_rate.sleep();
   }
-  //send zero velocity to topic
-  message.linear.x = 0.0;
-  publisher->publish(message);
-  rclcpp::spin_some(node);
-  loop_rate.sleep();
   rclcpp::shutdown();
   return 0;
+
 }
 
