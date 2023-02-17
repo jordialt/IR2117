43609661d232a2b08928e8ae6e7d40717e995401
jordialt <al416445@uji.es>
2023-02-17 17:29:55 +0100

    Update square.cpp

--- tb3_ws/src/square.cpp
+++ tb3_ws/src/square.cpp
@@ -7,19 +7,27 @@ using namespace std::chrono_literals;
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("square");
+  auto node = rclcpp::Node::make_shared("publisher");
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
   geometry_msgs::msg::Twist  message;
-  rclcpp::WallRate loop_rate(500ms);
+  rclcpp::WallRate loop_rate(10ms);
 
-  while (rclcpp::ok()) {
-    message.linear.x= 1.0;
-    message.linear.z=1.0;
+  int i=0, n=1000;
+  while (rclcpp::ok() && (i<n)) {
+    i++;
+    message.linear.x=0.1;
     publisher->publish(message);
     rclcpp::spin_some(node);
     loop_rate.sleep();
   }
+  message.linear.x=0.0;
+  publisher->publish(message);
+  rclcpp::spin_some(node);
+  loop_rate.sleep();
+
   rclcpp::shutdown();
   return 0;
+
 }
+
 
