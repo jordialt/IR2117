d6c9d18c972b1e6de7569c2b8c6204ac34aaa072
jordialt <al416445@uji.es>
2023-02-21 12:10:08 +0100

    Mode with maps: step 1

--- mode.cpp
+++ mode.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <vector>
 #include "utils.hpp"
+#include <map>
 
 int main() {
     double m=0;
     int element;
     std::vector<int> elements;
-    elements=ReadElements();
+    std::cin>> elements;
+    std::map<int,int> counts;
+    for (int value : elements){
+        counts[value]++;
+    }
     int n=elements.size();
     int count[n],max=0;
     for(int j=0; j<n; j++){
