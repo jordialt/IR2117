375e4c2c4337fcc30c429a203511f807183e9ead
jordialt <al416445@uji.es>
2023-02-21 11:52:52 +0100

    Operator overloading

--- utils.cc
+++ utils.cc
@@ -10,3 +10,14 @@ std::vector<int> ReadElements(){
     }
     return elements;
 }
+
+std::istream& operator>>(std::istream& in, std::vector<int>& v){
+    int element;
+    in>>element;
+    while(not in.eof()){
+        v.push_back(element);
+        in>>element;
+    }
+    return in;
+
+}
