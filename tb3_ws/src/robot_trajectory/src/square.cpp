7223a88abaf1d334a4d6d353fc074974db295a4f
jordialt <al416445@uji.es>
2023-02-23 13:15:23 +0100

    linear and angular iterations

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -9,13 +9,24 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("publisher");
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+  node->declare_parameter("linear_speed",0.1);
+  node->declare_parameter("angular_speed",3.1416/20);
+  
+  
   geometry_msgs::msg::Twist message;
   rclcpp::WallRate loop_rate(10ms);
+  
+ 
+  double linear_speed = node->get_parameter("linear_speed").get_parameter_value().get<double>();
+  double angular_speed = node->get_parameter("angular_speed").get_parameter_value().get<double>();
+  double linear_iterations = 1/(0.01*linear_speed);
+  double angular_iterations = 3.1416/(0.02*angular_speed);
+ 
   for(int j=0;j<4;j++){
-  	int i=0,n=1150;
+  	int i=0,n=linear_iterations;
   	while (rclcpp::ok() && (i<n)) {
 		i++;
-  		message.linear.x = 0.10;
+  		message.linear.x = linear_speed;
     	publisher->publish(message);
     	rclcpp::spin_some(node);
     	loop_rate.sleep();
@@ -27,11 +38,11 @@ int main(int argc, char * argv[])
   	loop_rate.sleep();
   
   	i=0;
-  	n=1150;
+  	n=angular_iterations;
 
   	while (rclcpp::ok() && (i<n)) {
 		i++;
-  		message.angular.z = 0.15707963268;
+  		message.angular.z = angular_speed;
     	publisher->publish(message);
     	rclcpp::spin_some(node);
     	loop_rate.sleep();
