8ca07b722d66aeec16188b888f9ae85614569533
jordialt <al416445@uji.es>
2023-02-23 13:21:11 +0100

    Square length

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -11,6 +11,7 @@ int main(int argc, char * argv[])
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
   node->declare_parameter("linear_speed",0.1);
   node->declare_parameter("angular_speed",3.1416/20);
+  node->declare_parameter("square_length",1.0);
   
   
   geometry_msgs::msg::Twist message;
@@ -19,7 +20,9 @@ int main(int argc, char * argv[])
  
   double linear_speed = node->get_parameter("linear_speed").get_parameter_value().get<double>();
   double angular_speed = node->get_parameter("angular_speed").get_parameter_value().get<double>();
-  double linear_iterations = 1/(0.01*linear_speed);
+  double square_length = node->get_parameter("square_length").get_parameter_value().get<double>();
+  
+  double linear_iterations = square_length/(0.01*linear_speed);
   double angular_iterations = 3.1416/(0.02*angular_speed);
  
   for(int j=0;j<4;j++){
