e91541799b7ac2fa41df4538567caa5696cf4c99
jordialt <al416445@uji.es>
2023-02-26 17:30:21 +0100

    Median with Eigen program

--- EigenProjects/median.cpp
+++ EigenProjects/median.cpp
@@ -1,4 +1,18 @@
 #include <Eigen/Dense>
 #include <iostream>
 #include "utils_eigen.hpp"
+#include <algorithm>
 
+int main(){
+    Eigen::VectorXd elements = ReadElementsEigen();
+    std::sort(elements.data(),elements.data()+elements.size());
+    int mid = elements.size()/2;
+    double median;
+    if (elements.size() % 2 == 0) {
+        median = (elements[mid] + elements[mid-1]) / 2;
+    } else {
+        median = elements[mid];
+    }
+    std::cout << "Median: " << median << std::endl;
+    return 0;
+}
