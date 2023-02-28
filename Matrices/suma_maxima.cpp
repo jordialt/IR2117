7f2d0b2a315ea9e18a68fb1f431e8bb0cae4e982
jordialt <al416445@uji.es>
2023-02-28 12:00:54 +0100

    suma max matriz V0

---
+++ Matrices/suma_maxima.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+using Eigen::MatriXd;
+
+int main{
+    int rows,cols;
+    double suma=0;
+    std::cout<<"Enter matrix size: "<<std::endl;
+    std::cin>>rows;
+    cols=rows;
+    MatrixXd m(rows,cols);
+    std::cout<<suma<<endl;
+    return 0;
+    
+}
+
