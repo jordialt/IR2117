064f815bafca1c7cdf28628aa91ab6dfbc6900a7
jordialt <al416445@uji.es>
2023-02-28 12:24:25 +0100

    suma_maxima V1
    Ask for the matrix size and enter the elements, then show on screen the resultant matrix

--- Matrices/suma_maxima.cpp
+++ Matrices/suma_maxima.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 #include <Eigen/Dense>
 
-using Eigen::MatriXd;
+using Eigen::MatrixXd;
 
-int main{
+int main(){
     int rows,cols;
     double suma=0;
     std::cout<<"Enter matrix size: "<<std::endl;
     std::cin>>rows;
     cols=rows;
     MatrixXd m(rows,cols);
-    std::cout<<suma<<endl;
+    std::cout<<"Enter elements(row-wise): "<<std::endl;
+    for(int i=0;i<rows;i++)
+        for(int j=0;j<cols;j++)
+            std::cin>>m(i,j);
+
+std::cout<<"The matrix is: "<<std::endl;
+for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            std::cout<<m(i,j);
+
+        };
+        std::cout<<std::endl;
+    }
     return 0;
-    
 }
 
