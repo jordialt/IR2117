8c36a34cf236a1fa7da277daa9e54256551cae41
jordialt <al416445@uji.es>
2023-03-23 13:27:54 +0100

    wandering - Version 5

--- tb3_ws/src/robot_trajectory/src/wandering.cpp
+++ tb3_ws/src/robot_trajectory/src/wandering.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <vector>
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
@@ -6,15 +7,27 @@
 using namespace std::chrono_literals;
 
 
+double min=1.5;
+
+
 void scan_callback(sensor_msgs::msg::LaserScan::SharedPtr msg){
     auto v = msg->ranges;
+    std::vector<float>  vx{};
     
     for(int i=0; i<10;i++)
-        std::cout<<v[i]<<std::endl;
+        vx.push_back(v[i]);
     
-    std::cout<<" "<<std::endl;
     for(int i=350;i<360;i++)
-        std::cout<<v[i]<<std::endl;
+        vx.push_back(v[i]);
+    
+    min = vx[0];
+    for(long unsigned int i=1;i<vx.size();i++) {
+        if(vx[i]<min) {
+            min=vx[i];
+        }
+    }
+    
+    std::cout<<"Mínimo: "<<min<<std::endl;
 
     
 }
@@ -31,12 +44,16 @@ int main(int argc, char * argv[])
   
   rclcpp::WallRate loop_rate(10ms);
 
-  while (rclcpp::ok()) {
-    message.linear.x = 0.0;
+  while (rclcpp::ok() && min>1.0) {
+    message.linear.x = 0.6;
     publisher->publish(message);
     rclcpp::spin_some(node);
     loop_rate.sleep();
   }
+  message.linear.x = 0.0;
+  publisher->publish(message);
+  rclcpp::spin_some(node);
+  loop_rate.sleep();
   rclcpp::shutdown();
   return 0;
 }
