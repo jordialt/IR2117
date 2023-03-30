b87fe1777e20b523e275a212deb7e177935bc5d7
jordialt <al416445@uji.es>
2023-03-30 12:30:44 +0200

    detector.cpp

---
+++ tb3_ws/src/obstacles/src/detector.cpp
@@ -0,0 +1,19 @@
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include <iostream>
+
+void callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+{
+  std::cout << msg->data << std::endl;
+}
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("detector");
+  auto subscription = node->create_subscription<sensor_msgs::msg::LaserScan>("scan", 10, callback);
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
+
