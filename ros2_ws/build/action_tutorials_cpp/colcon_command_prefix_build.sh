c534f2fa03f196dece66932607081f08ba85fd73
jordialt <al416445@uji.es>
2023-05-11 12:40:42 +0200

    Compiling and running the server

---
+++ ros2_ws/build/action_tutorials_cpp/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/usuario/Documentos/GitHub/IR2117/ros2_ws/install/action_tutorials_interfaces/share/action_tutorials_interfaces/package.sh"
