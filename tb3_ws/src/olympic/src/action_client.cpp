f9a66c29bef6649ab0e13ef2139da117500dcb3c
jordialt <al416445@uji.es>
2023-05-16 11:32:50 +0200

    Revert "Revert "Create action "rings" in package "olympic_interfaces"""
    This reverts commit 9714d7ad44bd331b7d33151fa02b0d6dd35b65de.

---
+++ tb3_ws/src/olympic/src/action_client.cpp
@@ -0,0 +1,120 @@
+#include <inttypes.h>
+#include <memory>
+#include "olympic_interfaces/action/rings.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include <iomanip>
+#include <sstream>
+
+// We need the action and goal handle classes, the chrono literals, and a pointer to the ROS node
+using Rings = 
+  olympic_interfaces::action::Rings;
+
+using GoalHandleRings =
+  rclcpp_action::ClientGoalHandle<Rings>;
+
+using namespace std::chrono_literals;
+
+rclcpp::Node::SharedPtr g_node = nullptr;
+
+// Assuming the goal was accepted by the server, it will start processing. Any feedback to the client will be handled by the feedback_callback
+void feedback_callback(GoalHandleRings::SharedPtr,
+  const std::shared_ptr<const Rings::Feedback> feedback)
+{
+  std::stringstream ss;
+  ss << std::setprecision(3) << "Circle n." << feedback->drawing_ring << " at "
+     << feedback->ring_angle << " degrees";
+  RCLCPP_INFO(
+    g_node->get_logger(),
+    ss.str().c_str());  // stringstream to string and then to char*
+}
+
+// The action client requires 3 things: 
+// The templated action type name: Rings
+// A ROS 2 node to add the action client to: g_node
+// The action name: "rings"
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  g_node = rclcpp::Node::make_shared("action_client");
+  g_node->declare_parameter("radius", 1.0);  // Radius parameter
+  double radius = g_node->get_parameter("radius").get_parameter_value().get<double>();
+  auto action_client = rclcpp_action::create_client<Rings>(
+    g_node, "rings");
+  
+  if (!action_client->wait_for_action_server(20s)) {
+    RCLCPP_ERROR(g_node->get_logger(), 
+      "Action server not available after waiting");
+    return 1;
+  }
+  // Populate a goal
+  auto goal_msg = Rings::Goal();
+  goal_msg.radius = radius;
+
+  // Ask server to achieve the goal
+  RCLCPP_INFO(g_node->get_logger(), 
+    "Sending goal");
+  auto send_goal_options = 
+    rclcpp_action::Client<Rings>::SendGoalOptions();
+  send_goal_options.feedback_callback = feedback_callback;
+  auto goal_handle_future = 
+    action_client->async_send_goal(goal_msg, send_goal_options);
+
+  // Wait until it's accepted or rejected
+  auto return_code = rclcpp::spin_until_future_complete(g_node,
+    goal_handle_future);
+  
+  // Exit in case of failure or if the goal has been rejected by the server
+  if (return_code != rclcpp::FutureReturnCode::SUCCESS)
+  {
+    RCLCPP_ERROR(g_node->get_logger(), 
+      "send goal call failed :(");
+    return 1;
+  }
+
+  GoalHandleRings::SharedPtr goal_handle = 
+    goal_handle_future.get();
+  if (!goal_handle) {
+    RCLCPP_ERROR(g_node->get_logger(), 
+      "Goal was rejected by server");
+    return 1;
+  }
+  
+  // Wait for the server to be done with the goal
+  auto result_future = 
+    action_client->async_get_result(goal_handle);
+
+  RCLCPP_INFO(g_node->get_logger(), "Waiting for result");
+
+  return_code = rclcpp::spin_until_future_complete(g_node, 
+    result_future);
+
+  if (return_code != rclcpp::FutureReturnCode::SUCCESS)
+  {
+    RCLCPP_ERROR(g_node->get_logger(), 
+      "get result call failed :(");
+    return 1;
+  }
+  GoalHandleRings::WrappedResult wrapped_result = 
+    result_future.get();
+
+  switch (wrapped_result.code) {
+    case rclcpp_action::ResultCode::SUCCEEDED:
+      break;
+    case rclcpp_action::ResultCode::ABORTED:
+      RCLCPP_ERROR(g_node->get_logger(), "Goal was aborted");
+      return 1;
+    case rclcpp_action::ResultCode::CANCELED:
+      RCLCPP_ERROR(g_node->get_logger(), "Goal was canceled");
+      return 1;
+    default:
+      RCLCPP_ERROR(g_node->get_logger(), "Unknown result code");
+      return 1;
+  }
+  RCLCPP_INFO(g_node->get_logger(), "Result received");
+
+  action_client.reset();
+  g_node.reset();
+  rclcpp::shutdown();
+  return 0;
+}
---
+++ tb3_ws/src/olympic/src/action_server.cpp
@@ -0,0 +1,187 @@
+#include <inttypes.h>
+#include <memory>
+#include <cmath>
+#include "olympic_interfaces/action/rings.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "turtlesim/srv/set_pen.hpp"
+#include "turtlesim/srv/teleport_absolute.hpp"
+
+using namespace std::chrono_literals;
+using turtlesim::srv::SetPen;
+using turtlesim::srv::TeleportAbsolute;
+
+using Rings = 
+  olympic_interfaces::action::Rings;
+
+using GoalHandleRings = 
+  rclcpp_action::ServerGoalHandle<Rings>;
+
+rclcpp::Node::SharedPtr node = nullptr;
+
+// Callback for handling new goals. This implementation accepts all goals
+rclcpp_action::GoalResponse handle_goal(
+  const rclcpp_action::GoalUUID & uuid, 
+  std::shared_ptr<const Rings::Goal> goal)
+{
+  RCLCPP_INFO(rclcpp::get_logger("server"), 
+    "Got goal request for a circle with radius %.*f", 2, goal->radius); // 2 decimals places
+  (void)uuid;
+  return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+}
+
+// Callback for dealing with cancellation. This implementation just tells the client it accepted the cancellation
+rclcpp_action::CancelResponse handle_cancel(
+  const std::shared_ptr<GoalHandleRings> goal_handle)
+{
+  RCLCPP_INFO(rclcpp::get_logger("server"), 
+    "Got request to cancel goal");
+  (void)goal_handle;
+  return rclcpp_action::CancelResponse::ACCEPT;
+}
+
+// This last set of callbacks accepts a new goal and starts processing it.
+void execute(const std::shared_ptr<GoalHandleRings>);
+
+void handle_accepted(
+  const std::shared_ptr<GoalHandleRings> goal_handle)
+{
+  std::thread{execute, goal_handle}.detach();
+}
+
+void execute(
+  const std::shared_ptr<GoalHandleRings> goal_handle)
+{
+  // Action related
+  RCLCPP_INFO(rclcpp::get_logger("server"), 
+    "Executing goal");
+  const auto goal = goal_handle->get_goal();
+  auto feedback = std::make_shared<Rings::Feedback>();
+  auto result = std::make_shared<Rings::Result>();
+  auto & ring_number = feedback->drawing_ring;  // Reference to drawing_ring
+  auto & ring_angle = feedback->ring_angle;     // and ring_angle
+  
+  // Ring related
+  float radius = goal->radius;  // Get the goal from the client
+  auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
+  geometry_msgs::msg::Twist message;
+  float v_angular = 1.2,
+        v_linear  = radius * v_angular;
+  int j, iterations = 2*M_PI / (v_angular * 0.1);
+  
+  rclcpp::WallRate loop_rate(100ms);
+  
+  // Iteration values
+	int r[5] = {0, 0, 223, 244, 0};
+	int g[5] = {129, 0, 0, 195, 159};
+	int b[5] = {200, 0, 36, 0, 61};
+	
+	double desplazamiento_x[5] = {-2.166667, 0, 2.166667, -1.166667, 1.166667};
+	double desplazamiento_y[5] = {0, 0, 0, -1, -1};
+	
+	// Initialize SetPen service
+	rclcpp::Client<SetPen>::SharedPtr client_set_pen =
+		node->create_client<SetPen>("/turtle1/set_pen");
+	auto request_set_pen = std::make_shared<SetPen::Request>();
+	auto result_set_pen = client_set_pen->async_send_request(request_set_pen);
+	
+	// Initialize TeleportAbsolute service
+	rclcpp::Client<TeleportAbsolute>::SharedPtr client_teleport_absolute =
+		node->create_client<TeleportAbsolute>("/turtle1/teleport_absolute");
+	auto request_teleport_absolute = std::make_shared<TeleportAbsolute::Request>();
+	auto result_teleport_absolute = client_teleport_absolute->async_send_request(request_teleport_absolute);
+  
+  // Begin the loop
+  for (int i = 0; i < 5; i++) {
+    if (goal_handle->is_canceling()) {
+      goal_handle->canceled(result);
+      RCLCPP_INFO(rclcpp::get_logger("server"), 
+      "Goal Canceled");
+      return;
+    }
+    // Change colors and disable pen
+		request_set_pen->r = r[i];
+		request_set_pen->g = g[i];
+		request_set_pen->b = b[i];
+		request_set_pen->width = 5;
+		request_set_pen->off = 1;
+    
+    result_set_pen = client_set_pen->async_send_request(request_set_pen);
+    
+    // Circle absolute position
+		request_teleport_absolute = std::make_shared<TeleportAbsolute::Request>();
+		request_teleport_absolute->x = 5.544445 + desplazamiento_x[i] * radius;
+		request_teleport_absolute->y = 5.544445 + desplazamiento_y[i] * radius;
+		request_teleport_absolute->theta = 0;
+
+    result_teleport_absolute = client_teleport_absolute->async_send_request(request_teleport_absolute);
+
+    // Publish feedback (ring and angle we are currently at)
+    ring_number = i+1;
+    ring_angle = 0;
+    
+    goal_handle->publish_feedback(feedback);
+    RCLCPP_INFO(rclcpp::get_logger("server"), 
+       "Publish Feedback");
+    
+    // Turn pen back on
+		request_set_pen->off = 0;
+		result_set_pen = client_set_pen->async_send_request(request_set_pen);
+		
+    // Draw the circle
+		j = 0;
+		while (rclcpp::ok() && (j <= iterations)) {
+			if (j == iterations/4) {  // 90 degrees
+				ring_angle = 90;
+				goal_handle->publish_feedback(feedback);
+    		RCLCPP_INFO(rclcpp::get_logger("server"), 
+       		"Publish Feedback");
+			} else if (j == iterations/2) {  // 180 degrees
+				ring_angle = 180;
+				goal_handle->publish_feedback(feedback);
+    		RCLCPP_INFO(rclcpp::get_logger("server"), 
+       		"Publish Feedback");
+			} else if (j == 3*iterations/4) {  // 270 degrees
+				ring_angle = 270;
+				goal_handle->publish_feedback(feedback);
+    		RCLCPP_INFO(rclcpp::get_logger("server"), 
+       		"Publish Feedback");
+			}
+			message.linear.x = v_linear;
+			message.angular.z = v_angular;
+			publisher->publish(message);
+			
+			j++;
+			loop_rate.sleep();
+		}
+		message.linear.x = 0.0;
+		message.angular.z = 0.0;
+		publisher->publish(message);
+		loop_rate.sleep();
+  }
+  
+  if (rclcpp::ok()) {  // When completed, if ROS is still running:
+    result->rings_completed = ring_number;
+    goal_handle->succeed(result);
+    RCLCPP_INFO(rclcpp::get_logger("server"), 
+      "All five rings completed");
+  }
+}
+
+
+// Main function
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  node = rclcpp::Node::make_shared("action_server");
+  auto action_server = 
+    rclcpp_action::create_server<Rings>(node,
+      "rings",
+      handle_goal,
+      handle_cancel,
+      handle_accepted);
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
