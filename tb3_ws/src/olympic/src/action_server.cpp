735bef21ce6011a002b07d5f04195456e5d45c0c
jordialt <al416445@uji.es>
2023-05-18 12:41:51 +0200

    Add the code to the server for drawing the circles

--- tb3_ws/src/olympic/src/action_server.cpp
+++ tb3_ws/src/olympic/src/action_server.cpp
@@ -45,22 +45,114 @@ void handle_accepted(
 
 void execute(const std::shared_ptr<GoalHandleRings> goal_handle)
 {
-  // Action related
+  // Action 
   RCLCPP_INFO(rclcpp::get_logger("server"), "Executing goal");
   const auto goal = goal_handle->get_goal();
   auto feedback = std::make_shared<Rings::Feedback>();
   auto result = std::make_shared<Rings::Result>();
-
-  // Ring related
-  float radius = goal->radius;  // Get the goal from the client
   
-  // Print the received radius
-  RCLCPP_INFO(rclcpp::get_logger("server"), "Received goal with radius: %.*f", 2, radius);
+  auto & ring_number = feedback->drawing_ring;  // Reference to drawing_ring
+  auto & ring_angle = feedback->ring_angle;     // and ring_angle
+  
+  // Ring 
+  float radius = goal->radius;  // Get the goal from the client
+  auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
+  geometry_msgs::msg::Twist message;
+  float v_angular = 1.2,
+        v_linear  = radius * v_angular;
+  int j, iterations = 2*M_PI / (v_angular * 0.1);
+  
+  rclcpp::WallRate loop_rate(100ms);
+  
+  // Iteration values
+	int r[5] = {0, 0, 223, 244, 0};
+	int g[5] = {129, 0, 0, 195, 159};
+	int b[5] = {200, 0, 36, 0, 61};
+	
+	double desplazamiento_x[5] = {-2.166667, 0, 2.166667, -1.166667, 1.166667};
+	double desplazamiento_y[5] = {0, 0, 0, -1, -1};
+	
+	// Init SetPen service
+	rclcpp::Client<SetPen>::SharedPtr client_set_pen =
+		node->create_client<SetPen>("/turtle1/set_pen");
+	auto request_set_pen = std::make_shared<SetPen::Request>();
+	auto result_set_pen = client_set_pen->async_send_request(request_set_pen);
+	
+	// Init TeleportAbsolute service
+	rclcpp::Client<TeleportAbsolute>::SharedPtr client_teleport_absolute =
+		node->create_client<TeleportAbsolute>("/turtle1/teleport_absolute");
+	auto request_teleport_absolute = std::make_shared<TeleportAbsolute::Request>();
+	auto result_teleport_absolute = client_teleport_absolute->async_send_request(request_teleport_absolute);
+  
+  // Begin loop
+  for (int i = 0; i < 5; i++) {
+    if (goal_handle->is_canceling()) {
+      goal_handle->canceled(result);
+      RCLCPP_INFO(rclcpp::get_logger("server"), 
+      "Goal Canceled");
+      return;
+    }
+    // Change colors & disable pen
+		request_set_pen->r = r[i];
+		request_set_pen->g = g[i];
+		request_set_pen->b = b[i];
+		request_set_pen->width = 5;
+		request_set_pen->off = 1;
+    
+    result_set_pen = client_set_pen->async_send_request(request_set_pen);
+    
+    // Absolute position
+		request_teleport_absolute = std::make_shared<TeleportAbsolute::Request>();
+		request_teleport_absolute->x = 5.544445 + desplazamiento_x[i] * radius;
+		request_teleport_absolute->y = 5.544445 + desplazamiento_y[i] * radius;
+		request_teleport_absolute->theta = 0;
 
-  // Set the result and send success
-  result->success = true;
-  goal_handle->succeed(result);
+    result_teleport_absolute = client_teleport_absolute->async_send_request(request_teleport_absolute);
+
+
+		request_set_pen->off = 0;
+		result_set_pen = client_set_pen->async_send_request(request_set_pen);
+		
+     
+		j = 0;
+		while (rclcpp::ok() && (j <= iterations)) {
+			if (j == iterations/4) {  
+				ring_angle = 90;
+				goal_handle->publish_feedback(feedback);
+    		RCLCPP_INFO(rclcpp::get_logger("server"), 
+       		"Publish Feedback");
+			} else if (j == iterations/2) { 
+				ring_angle = 180;
+				goal_handle->publish_feedback(feedback);
+    		RCLCPP_INFO(rclcpp::get_logger("server"), 
+       		"Publish Feedback");
+			} else if (j == 3*iterations/4) { 
+				ring_angle = 270;
+				goal_handle->publish_feedback(feedback);
+    		RCLCPP_INFO(rclcpp::get_logger("server"), 
+       		"Publish Feedback");
+			}
+			message.linear.x = v_linear;
+			message.angular.z = v_angular;
+			publisher->publish(message);
+			
+			j++;
+			loop_rate.sleep();
+		}
+		message.linear.x = 0.0;
+		message.angular.z = 0.0;
+		publisher->publish(message);
+		loop_rate.sleep();
+  }
+  
+  if (rclcpp::ok()) {  // When completed, if ROS is still running:
+    result->rings_completed = ring_number;
+    goal_handle->succeed(result);
+    RCLCPP_INFO(rclcpp::get_logger("server"), 
+      "All five rings completed");
+  }
 }
+
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
